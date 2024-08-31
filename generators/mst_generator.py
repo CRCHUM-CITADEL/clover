67636ffbff5333022a50b0e4d7fc7f5d69f3328e
Yue Qi <yue.qi.chum@ssss.gouv.qc.ca>
2024-08-30 20:35:08 -0400

    Fix output error

--- generators/mst_generator.py
+++ generators/mst_generator.py
@@ -191,8 +191,8 @@ class MSTGenerator(Generator):
 
         for idx, col in enumerate(self._metadata["continuous"]):
             half_bin_width = (
-                self._kbins.bin_edges_[idx][1] - self._kbins.bin_edges_[idx][0] / 2
-            )
+                self._kbins.bin_edges_[idx][1] - self._kbins.bin_edges_[idx][0]
+            ) / 2
 
             samples[col] = samples[col] + np.random.uniform(
                 -half_bin_width, half_bin_width, size=len(samples)

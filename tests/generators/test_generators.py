0ba7e1ee1b0a0f866851fcc1dce1230464cc5ef1
Yue Qi <yue.qi.chum@ssss.gouv.qc.ca>
2024-09-27 09:53:54 -0400

    Add test cases for non-DP and DP generators

--- tests/generators/test_generators.py
+++ tests/generators/test_generators.py
@@ -1,5 +1,5 @@
 import pytest  # standard library
-from typing import Type, Tuple
+from typing import Type
 import tempfile
 from pathlib import Path
 from inspect import getfullargspec
@@ -20,25 +20,26 @@ from generators.ctabgan_generator import CTABGANGenerator
 
 
 @pytest.mark.parametrize(
-    "generator",
+    "generator_non_dp",
     [
         DataSynthesizerGenerator,
         SynthpopGenerator,
         SmoteGenerator,
-        MSTGenerator,
         CTGANGenerator,
         TVAEGenerator,
         CTABGANGenerator,
         FinDiffGenerator,
     ],
 )
-def test_generation(
-    generator: Type[Generator], df_wbcd: dict[str, pd.DataFrame], metadata_wbcd: dict
+def test_generation_non_dp(
+    generator_non_dp: Type[Generator],
+    df_wbcd: dict[str, pd.DataFrame],
+    metadata_wbcd: dict,
 ) -> None:
     """
-    Check the generation process.
+    Check the generation process for non differentially private generators.
 
-    :param generator: the class of the generator to test
+    :param generator_non_dp: the class of the generator to test
     :param df_wbcd: the real Wisconsin Breast Cancer Dataset fixture, split into **train** and **test** sets
     :param metadata_wbcd: the wbcd metadata fixture
     :return: *None*
@@ -55,32 +56,139 @@ def test_generation(
             "metadata": metadata_wbcd,
             "random_state": 0,
             "generator_filepath": None,
+            "epsilon": None,
             "candidate_keys": None,  # datasynthesizer
-            "epsilon": 1,  # datasynthesizer / synthpop / smote / MST / ctgan/ tvae / ctabganplus / findiff
             "degree": 2,  # datasynthesizer
             "variables_order": None,  # synthpop
             "min_samples_leaf": 5,  # synthpop
             "max_depth": None,  # synthpop
+            "k_neighbors": 5,  # smote
+            "discriminator_steps": 2,  # ctgan
+            "epochs": 2,  # ctgan / tvae / ctabganplus / findiff
+            "batch_size": 100,  # ctgan / tvae / ctabganplus / findiff
+            "verbose": 0,  # ctgan
+            "max_physical_batch_size": 126,  # tvae
+            "compress_dims": (249, 249),  # tvae
+            "decompress_dims": (249, 249),  # tvae
+            "mixed_columns": None,  # ctabganplus
+            "log_columns": None,  # ctabganplus
+            "integer_columns": None,  # ctabganplus
+            "class_dim": (256, 256, 256, 256),  # ctabganplus
+            "random_dim": 100,  # ctabganplus
+            "num_channels": 64,  # ctabganplus
+            "l2scale": 1e-5,  # ctabganplus
+            "learning_rate": 1e-4,  # findiff
+            "diffusion_steps": 50,  # findiff
+            "mpl_layers": [1024, 1024, 1024, 1024],  # findiff
+            "activation": "lrelu",  # findiff
+            "dim_t": 64,  # findiff
+            "cat_emb_dim": 2,  # findiff
+            "diff_beta_start_end": [1e-4, 0.02],  # findiff
+            "scheduler": "linear",  # findiff
+            # Parameters not applicable to non-dp generators
+            "preprocess_metadata": None,
+            "n_bins": None,  # synthpop
             "methods": None,  # synthpop
-            "bounds": None,  # synthpop / smote / findiff
             "prediction_matrix": None,  # synthpop
             "n_parents": None,  # synthpop
-            "k_neighbors": 5,  # smote
+            "l_connectivity": None,  # smote
+            "nu": None,  # smote
+            "r": None,  # smote
+            "delta": None,  # ctgan / tvae / ctabganplus / findiff
+            "max_grad_norm": None,  # ctgan / tvae / ctabganplus / findiff
+        }
+
+        # Select only the expected instance parameters
+        args = getfullargspec(generator_non_dp).args[1:]  # remove self
+        gen = generator_non_dp(*[d[arg] for arg in args])
+
+        # Preprocess and fit the generator
+        gen.preprocess()
+        gen.fit(save_path=temp_dir)
+
+        # Check that the generator is saved
+        num_files = len(list(temp_dir.glob("*")))
+        assert (
+            num_files >= 2
+        ), "The generator should have been saved"  # with the datafile
+
+        # Generate the samples
+        df_synth = gen.sample(save_path=temp_dir, num_samples=len(df_wbcd["train"]))
+
+        # Check that the generated samples are consistent
+        num_files_plusone = len(list(Path(temp_dir).glob("*")))
+        assert num_files_plusone > num_files, "The samples should have been saved"
+        assert (
+            df_wbcd["train"].shape == df_synth.shape
+        ), "Datasets must have the same shape"
+        assert set(df_wbcd["train"].columns) == set(
+            df_synth.columns
+        ), "Datasets must have the same columns"
+
+
+@pytest.mark.parametrize(
+    "generator_dp",
+    [
+        DataSynthesizerGenerator,
+        SynthpopGenerator,
+        SmoteGenerator,
+        MSTGenerator,
+        CTGANGenerator,
+        TVAEGenerator,
+        CTABGANGenerator,
+        FinDiffGenerator,
+    ],
+)
+def test_generation_dp(
+    generator_dp: Type[Generator],
+    df_wbcd: dict[str, pd.DataFrame],
+    metadata_wbcd: dict,
+    preprocess_metadata_wbcd: dict,
+) -> None:
+    """
+    Check the generation process for differentially private generators.
+
+    :param generator_dp: the class of the generator to test
+    :param df_wbcd: the real Wisconsin Breast Cancer Dataset fixture, split into **train** and **test** sets
+    :param metadata_wbcd: the wbcd metadata fixture
+    :param preprocess_metadata_wbcd: the wbcd preprocessing metadata fixture
+    :return: *None*
+    """
+
+    # Instance parameters
+    with tempfile.TemporaryDirectory() as temp_dir:  # no need to keep the generated files
+        temp_dir = Path(temp_dir)
+        datapath = temp_dir / "real_data.csv"
+        df_wbcd["train"].to_csv(datapath, index=False)
+
+        d = {
+            "df": df_wbcd["train"],
+            "metadata": metadata_wbcd,
+            "random_state": 0,
+            "generator_filepath": None,
+            "epsilon": 1,
+            "preprocess_metadata": preprocess_metadata_wbcd,
+            "candidate_keys": None,  # datasynthesizer
+            "degree": 2,  # datasynthesizer
+            "variables_order": None,  # synthpop
+            "max_depth": 3,  # synthpop
+            "n_bins": 10,  # synthpop
+            "methods": None,  # synthpop
+            "prediction_matrix": None,  # synthpop
+            "n_parents": 2,  # synthpop
             "l_connectivity": 2,  # smote
             "nu": 0.5,  # smote
             "cat_emb_dim": 2,  # smote / findiff
             "r": 1,  # smote
             "delta": 1e-9,  # MST / ctgan / tvae / ctabganplus / findiff
-            "preprocess_metadata": None,  # ctgan / tvae / ctabganplus
             "discriminator_steps": 2,  # ctgan
-            "epochs": 50,  # ctgan / tvae / ctabganplus / findiff
+            "epochs": 2,  # ctgan / tvae / ctabganplus / findiff
             "batch_size": 100,  # ctgan / tvae / ctabganplus / findiff
-            "preprocess_epsilon_pp": None,  # ctgan / tvae / ctabganplus
             "max_grad_norm": 1,  # ctgan / tvae / ctabganplus / findiff
             "verbose": 0,  # ctgan
+            "max_physical_batch_size": 126,  # tvae
             "compress_dims": (249, 249),  # tvae
             "decompress_dims": (249, 249),  # tvae
-            "max_physical_batch_size": 126,  # tvae
             "mixed_columns": None,  # ctabganplus
             "log_columns": None,  # ctabganplus
             "integer_columns": None,  # ctabganplus
@@ -95,11 +203,14 @@ def test_generation(
             "dim_t": 64,  # findiff
             "diff_beta_start_end": [1e-4, 0.02],  # findiff
             "scheduler": "linear",  # findiff
+            # Parameters not applicable to dp generators
+            "min_samples_leaf": None,  # synthpop
+            "k_neighbors": None,  # smote
         }
 
         # Select only the expected instance parameters
-        args = getfullargspec(generator).args[1:]  # remove self
-        gen = generator(*[d[arg] for arg in args])
+        args = getfullargspec(generator_dp).args[1:]  # remove self
+        gen = generator_dp(*[d[arg] for arg in args])
 
         # Preprocess and fit the generator
         gen.preprocess()
